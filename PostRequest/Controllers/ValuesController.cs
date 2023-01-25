2512920f018b511cec7a28234f1e1cc6fd2088fa
Sevval Ciran <sevvalciran@anadoluhayat.com.tr>
2023-01-25 16:02:02 +0300

    Add project files.

---
+++ PostRequest/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PostRequest.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] Data veri)
+        {
+            return Ok(veri);
+        }
+       
+    }
+}
---
+++ PostRequest/Data.cs
@@ -0,0 +1,11 @@
+﻿namespace PostRequest
+{
+    public class Data
+    {
+        public int CONCLUSION_DATE { get; set; }
+        public int POLICY_NO { get; set; }
+        
+    }
+}
+
+
