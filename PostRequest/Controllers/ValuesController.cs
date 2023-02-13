2ece408db205217f6b64078ad4d06a8b8125f353
Sevval Ciran <sevvalciran@anadoluhayat.com.tr>
2023-02-13 14:23:14 +0300

    deserialize

--- PostRequest/Controllers/ValuesController.cs
+++ PostRequest/Controllers/ValuesController.cs
@@ -1,6 +1,18 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Nancy;
+using Nancy.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Dynamic;
+using System.Reflection.Metadata;
+using System.Runtime.Serialization.Json;
+using System.Security.Principal;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
 namespace PostRequest.Controllers
 {
@@ -8,12 +20,52 @@ namespace PostRequest.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+
         [HttpPost]
-        public IActionResult Post([FromBody] Data veri)
+        public IActionResult Post([FromBody]  dynamic jsonx)
         {
 
-            return Ok(veri);
+
+            //  var data = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ExpandoObject>>(new ExpandoObject());
+          //  string jsona = System.Text.Json.JsonSerializer.Serialize(jsonx);
+
+           Data dataa = JsonConvert.DeserializeObject<Data>(jsonx.ToString());
+
+            var x = dataa;
+            return Ok(dataa);
+           
+
+
+            // var userDetails = GetJsonGenericType<dynamic>(jsonx);
+
+            // var data = JsonSerializer.Deserialize(jsonx.ToString());
+
+            //var Data = JsonSerializer.Deserialize(jsonx);
+            //var datas = JsonConvert.DeserializeObject<dynamic>(data.ToString());
+
+            //  Employee employee = JsonConvert.DeserializeObject<Employee>(jsonx.ToString());
+            //dynamic jsonObj = JsonConvert.DeserializeObject(jsonx);
+
+            //  var jsonString = ((System.Text.Json.JsonDocument)jsonx).RootElement.GetRawText();
+            ///  return JsonConvert.DeserializeObject<Data>(jsonString);
+
+            // Data datax = JsonConvert.DeserializeObject<Data>(jsonx.ToString());
+            //
+            // var objDeserializeObject = JsonConvert.DeserializeObject<Data>(jsonx.ToString());
+
+            //  var person = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(jsonx.ToString());
+
+            // dynamic deserializedObject = JsonConvert.DeserializeObject(jsonx.ToString());
+
+            //  Console.WriteLine(data);
+
         }
-       
+
+        
+
+
     }
+
+   
 }
+
--- PostRequest/Data.cs
+++ PostRequest/Data.cs
@@ -1,11 +1,14 @@
-﻿namespace PostRequest
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace PostRequest
 {
-    public class Data
-    {
-        public int CONCLUSION_DATE { get; set; }
-        public int POLICY_NO { get; set; }
-        
-    }
+    public class Data { 
+   
+    public string POLICY_NO { get; set; }
+    public string CONCLUSION_DATE { get; set; }
 }
 
 
+
+}
